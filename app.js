f396e6cbaec796996e3cb2e07b0ffd4691b129f1
Anshuman Shukla <anshumanshukla1800@gmail.com>
2024-02-08 12:29:59 +0530

    lesson2

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 const heading = React.createElement("div", { id: "parent1" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Hello World from React"),
-    React.createElement("h2", {}, "Hello World from React"),
+    React.createElement("h1", {}, "Hello fdfdgdgdgdgdgdg World from React"),
+    React.createElement("h2", {}, "Hello  React"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "Hello World from React"),
-    React.createElement("h2", {}, "Hello World from React"),
+    React.createElement("h2", {}, "Hello React"),
   ]),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
